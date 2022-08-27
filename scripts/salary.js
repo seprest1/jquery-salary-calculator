935757a058f59b3b2e13f8c20f62c6ae166590c5
Sarah Preston <seprest@gmail.com>
2022-08-27 09:05:48 -0500

    added button that changes table data to match input values

--- scripts/salary.js
+++ scripts/salary.js
@@ -1,2 +1,26 @@
-$(document).ready(function(){});
+$(document).ready(clickHandler);
 
+function clickHandler(){
+    $(`#submitButton`).on(`click`, addEmployee);
+}
+
+let employeeData = {
+    firstName: $(`firstNameInput`).val(),
+    lastName: $(`lastNameInput`).val(),
+    employeeId: $(`employeeIdInput`).val(),
+    title: $(`titleInput`).val(),
+    salary: $(`salaryInput`).val()
+}
+
+function addEmployee(){
+    $(`.employeeTable`).append(`
+    <tr>
+        <td>${employeeData.firstName}</td>
+        <td>${employeeData.lastName}</td>
+        <td>${employeeData.employeeId}</td>
+        <td>${employeeData.title}</td>
+        <td>${employeeData.salary}</td>
+        <td>Delete</td>
+    </tr>`);
+    console.log(`hi`);
+}
