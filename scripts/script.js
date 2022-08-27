399d692af8722086cc2f72e058c795ff311b1a21
Sarah Preston <seprest@gmail.com>
2022-08-27 18:06:19 -0500

    added some more css

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,7 @@ $(document).ready(clickHandler);
 
 function clickHandler(){
     $(`#submitButton`).on(`click`, addEmployee); //when button is clicked, add the employee
-    $(".employeeTable").on("click", ".deleteButton", function() {
-        $(this).closest("tr").remove();
-     }); //when button is clicked, delete employee
+    $(".employeeTable").on("click", ".deleteButton", $(`#tableSalary`), deleteEmployee); //when button is clicked, delete employee
 }
 
 let employeeArray = [];
@@ -17,26 +15,26 @@ function addEmployee(){
         title: $(`#titleInput`).val(),
         salary: Number($(`#salaryInput`).val()),
     }
-
     employeeArray.push(employeeData); //add employeeData object to array of employees
     
     $(`.employeeTable`).append(`
-    <tr id = "${employeeData.employeeId}">
+    <tr id= "${employeeData.employeeId}">
         <td>${employeeData.firstName}</td>
         <td>${employeeData.lastName}</td>
         <td>${employeeData.employeeId}</td>
         <td>${employeeData.title}</td>
-        <td>${employeeData.salary}</td>
+        <td class="employeeSalary" >${employeeData.salary}</td>
         <td><button class="deleteButton">X</button></td>
     </tr>`);  //add new employee data to table
+
+    $(`.employeeSalary`).data(`salary ${employeeData.employeeId}`, $(`#salaryInput`).val());
+    console.log(`Employee Salary:`, $(`.employeeSalary`).data());
 
     $(`.inputs`).val('');  //empty imputs
     calculateCosts();
 }
 
-function appendTable(){
-    
-}
+console.log(`Employee Salary:`, $(`.employeeSalary`).data());
 
 function calculateCosts(){
     $(`#monthlyCosts`).empty(); //empty out monthly costs
@@ -46,7 +44,12 @@ function calculateCosts(){
     }  //end loop
     if (totalCost > 20000){
         $(`#monthlyBudget`).css("background-color", "red"); //if salary cost exceeds budget, turn red
-        console.log('red');
     }
     $(`#monthlyCosts`).append(`${totalCost}`); //add new monthly total to DOM
 }
+
+function deleteEmployee(){
+    $(this).closest("tr").remove();
+ }
+
+ 
